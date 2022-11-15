e93ce9a87025879fdb19f1f1e7a90cb83f9ba420
AgosHerdeiro <aherdeiro@alumno.unlam.edu.ar>
2022-11-15 17:07:37 -0300

    home

---
+++ js/script.js
@@ -0,0 +1,99 @@
+$(document).ready(function () {
+    $('.carousel').carousel();
+
+});
+
+$(document).ready(function () {
+    var estilo1 = $('#detalle1').css('zIndex');
+    if (estilo1 == 0) {
+        $('.det1').show();
+    } else {
+        $('.det1').hide();
+    }
+});
+
+$('.carousel').mousedown(function () {
+    var estilo1 = $('#detalle1').css('zIndex');
+    if (estilo1 == 0) {
+        $('.det1').show();
+    } else {
+        $('.det1').hide();
+    }
+});
+
+$(document).ready(function () {
+    var estilo2 = $('#detalle2').css('zIndex');
+    if (estilo2 == 0) {
+        $('.det2').show();
+        console.log('1');
+    } else {
+        $('.det2').hide();
+        console.log('2');
+    }
+});
+
+$('.carousel').mousedown(function () {
+    var estilo2 = $('#detalle2').css('zIndex');
+    if (estilo2 == 0) {
+        $('.det2').show();
+    } else {
+        $('.det2').hide();
+    }
+});
+
+$(document).ready(function () {
+    var estilo3 = $('#detalle3').css('zIndex');
+    if (estilo3 == 0) {
+        $('.det3').show();
+    } else {
+        $('.det3').hide();
+    }
+});
+
+$('.carousel').mousedown(function () {
+    var estilo3 = $('#detalle3').css('zIndex');
+    if (estilo3 == 0) {
+        $('.det3').show();
+    } else {
+        $('.det3').hide();
+    }
+});
+
+$(document).ready(function () {
+    var estilo4 = $('#detalle4').css('zIndex');
+    if (estilo4 == 0) {
+        $('.det4').show();
+    } else {
+        $('.det4').hide();
+    }
+});
+
+$('.carousel').mousedown(function () {
+    var estilo4 = $('#detalle4').css('zIndex');
+    if (estilo4 == 0) {
+        $('.det4').show();
+    } else {
+        $('.det4').hide();
+    }
+});
+
+$(document).ready(function () {
+    var estilo5 = $('#detalle5').css('zIndex');
+    if (estilo5 == 0) {
+        $('.det5').show();
+    } else {
+        $('.det5').hide();
+    }
+});
+
+$('.carousel').mousedown(function () {
+    var estilo5 = $('#detalle5').css('zIndex');
+    if (estilo5 == 0) {
+        $('.det5').show();
+    } else {
+        $('.det5').hide();
+    }
+});
+
+
+
